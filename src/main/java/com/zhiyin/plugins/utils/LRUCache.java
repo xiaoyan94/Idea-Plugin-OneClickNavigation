74546416c1d621270462cfd224972abe0a99f5e5
xiaoyan94 <ruguo_0904@163.com>
2024-07-04 19:25:38 +0800

    docs(LRUCache): add javadoc comment for class explanation
    Add a javadoc comment at the beginning of the LRUCache class to provide a brief
    introduction and explanation of the class's purpose and functionality. This
    improves code clarity and helps developers understand the role of LRUCache in
    the context of the application.

--- src/main/java/com/zhiyin/plugins/utils/LRUCache.java
+++ src/main/java/com/zhiyin/plugins/utils/LRUCache.java
@@ -2,7 +2,9 @@ package com.zhiyin.plugins.utils;
 
 import java.util.LinkedHashMap;
 
-
+/**
+ * LRU Cache 是 LinkedHashMap 的子类，实现了一个 LRU 策略的缓存，当缓存满时，会自动删除最老的缓存项。
+ */
 public final class LRUCache<K, V> extends LinkedHashMap<K, V> {
     private final int MAX_SIZE;
 
